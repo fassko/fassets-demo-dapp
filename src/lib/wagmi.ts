4583b774b5845771cab674bd57506dbf7704dec1
Kristaps Grinbergs <kristaps@flare.network>
2025-10-09 18:28:55 +0300

    Refactor wagmi configuration by removing walletConnect connector to streamline connection options

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,15 +1,11 @@
 import { flare, flareTestnet, songbird, songbirdTestnet } from 'wagmi/chains';
-import { injected, metaMask, walletConnect } from 'wagmi/connectors';
+import { injected, metaMask } from 'wagmi/connectors';
 
 import { createConfig, http } from 'wagmi';
 
 export const config = createConfig({
   chains: [flare, flareTestnet, songbird, songbirdTestnet],
-  connectors: [
-    injected(),
-    metaMask(),
-    walletConnect({ projectId: 'YOUR_WALLET_CONNECT_PROJECT_ID' }),
-  ],
+  connectors: [injected(), metaMask()],
   transports: {
     [flare.id]: http(), // Flare Mainnet (Chain ID: 14)
     [flareTestnet.id]: http(), // Coston2 Testnet (Chain ID: 114)
